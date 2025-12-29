3c004e6c230bce104bc2d49a85fc18e1548ac07c
zyf1988 <zhouyuanfeng@foxmail.com>
2025-12-29 23:21:21 +0800

    update the version

--- QuickInfoTest/comment/CommentTranslate.cs
+++ QuickInfoTest/comment/CommentTranslate.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using CommentTranslator.Client;
 
 namespace CommentTranslator22.Popups.QuickInfo.Comment
 {
